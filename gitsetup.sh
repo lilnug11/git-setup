05ca6c610598c23ccb3992bed33613c346b48e9d
lilnug11 <lilnug11@gmail.com>
2023-02-14 09:13:10 -0500

    Update gitsetup.sh

--- gitsetup.sh
+++ gitsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git config --global pull.rebase false
-git config --global user.name "your-username"
-git config --global user.email "your-email@gmail.com"
+git config --global user.name "lilnug11"
+git config --global user.email "lilnug11@gmail.com"
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=31536000'
